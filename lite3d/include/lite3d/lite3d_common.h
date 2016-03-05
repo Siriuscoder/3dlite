076c25e77087b10428f121a5601216c5b36bff0e
Nikita Korolev <siriusnick@gmail.com>
2016-03-05 11:16:19 +0300

    FUNCTION marco fix

--- lite3d/include/lite3d/lite3d_common.h
+++ lite3d/include/lite3d/lite3d_common.h
@@ -87,21 +87,7 @@
 #   define COMPILER_VERSION "UNKNOWN"
 #endif
 
-#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
-#   define LITE3D_CURRENT_FUNCTION __PRETTY_FUNCTION__
-#elif defined(__DMC__) && (__DMC__ >= 0x810)
-#   define LITE3D_CURRENT_FUNCTION __PRETTY_FUNCTION__
-#elif defined(__FUNCSIG__)
-#   define LITE3D_CURRENT_FUNCTION __FUNCSIG__
-#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-#   define LITE3D_CURRENT_FUNCTION LITE3D_CURRENT_FUNCTION
-#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-#   define LITE3D_CURRENT_FUNCTION __FUNC__
-#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-#   define LITE3D_CURRENT_FUNCTION __func__
-#else
-#   define LITE3D_CURRENT_FUNCTION "(unknown)"
-#endif
+#define LITE3D_CURRENT_FUNCTION __func__
 
 #define LITE3D_CEXPORT  LITE3D_EXPORT LITE3D_EXTERN
 #define LITE3D_CPPEXPORT  LITE3D_CLASS_EXPORT LITE3D_EXTERN
