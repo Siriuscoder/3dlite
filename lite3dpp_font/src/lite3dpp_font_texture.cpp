6b4a013219a89aeb025e2ba5f1a279c0b09307aa
Nikita Korolev <siriusnick@gmail.com>
2018-12-12 18:35:56 +0300

    fix SDL logger M_PI warning, added some helper functions

--- lite3dpp_font/src/lite3dpp_font_texture.cpp
+++ lite3dpp_font/src/lite3dpp_font_texture.cpp
@@ -15,11 +15,11 @@
  *	You should have received a copy of the GNU General Public License
  *	along with Lite3D.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
-#include <SDL_log.h>
-
 #include <font.h>
 #include <texture.h>
 #include <log/logger.h>
+
+#include <SDL_log.h>
 
 #include <lite3dpp/lite3dpp_main.h>
 #include <lite3dpp_font/lite3dpp_font_texture.h>
