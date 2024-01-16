84a29fbd43a79694cf8d6238769c0ce7a605d942
Nikita Korolev <siriusnick@gmail.com>
2024-01-16 23:43:02 +0600

    расширение сцены vault_111, добавляем фурнитуру

--- media/vault_111/shaders/sources/mrt.fs
+++ media/vault_111/shaders/sources/mrt.fs
@@ -1,4 +1,21 @@
+#if defined(MRT_WITH_EMISSION)
+
 uniform sampler2D Albedo;
+uniform sampler2D Emission;
+uniform float EmissionStrength;
+
+#elif defined(MRT_WITH_EMISSION_SOLID)
+
+uniform vec4 Emission;
+uniform float EmissionStrength;
+uniform float Roughness;
+uniform float Specular;
+
+#else
+
+uniform sampler2D Albedo;
+
+#endif
 
 layout(location = 0) out vec4 fragWCoord;
 layout(location = 1) out vec4 fragWNormal;
@@ -14,14 +31,47 @@ vec3 GetSpecular(vec2 iuv);
 
 void main()
 {
+#if defined(MRT_WITH_EMISSION)
+    // sampling albedo 
     vec3 albedo = texture(Albedo, iuv).rgb;
+    // sampling emission 
+    vec3 emission = texture(Emission, iuv).rgb * EmissionStrength;
+    // Get Fixed normal
+    vec3 normal = GetFixedWorldNormal(itbn, iuv);
+    // Get Specular params
+    vec3 specular = GetSpecular(iuv);
 
+#elif defined(MRT_WITH_EMISSION_SOLID)
+
+    vec3 albedo = Emission.rgb;
+    vec3 emission = Emission.rgb * EmissionStrength;
+    vec3 normal = itbn[2];
+    vec3 specular = vec3(Specular, Roughness, 0.0);
+
+#else
+
+    // sampling albedo 
+    vec3 albedo = texture(Albedo, iuv).rgb;
+    // Non emission material 
+    vec3 emission = vec3(0.0);
+    // Get Fixed normal
+    vec3 normal = GetFixedWorldNormal(itbn, iuv);
+    // Get Specular params
+    vec3 specular = GetSpecular(iuv);
+
+#endif
+
+// Additional modifiers
 #ifdef PALETE_YELLOW
     albedo *= PALETE_YELLOW;
 #endif
 
+#ifdef EMISSION_GREEN
+    emission *= EMISSION_GREEN;
+#endif
+
     fragWCoord = vec4(ivv, gl_FragCoord.z / gl_FragCoord.w);
-    fragWNormal = vec4(GetFixedWorldNormal(itbn, iuv), 0.0);
-    fragAlbedo = vec4(albedo, 0.0);
-    fragSpecular = vec4(GetSpecular(iuv), 0.0);
+    fragWNormal = vec4(normal, emission.r);
+    fragAlbedo = vec4(albedo, emission.g);
+    fragSpecular = vec4(specular, emission.b);
 }
