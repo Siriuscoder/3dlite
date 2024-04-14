4b42961c9d056f3e85cc0997ef5ab3149a87860e
Nikita Korolev <siriusnick@gmail.com>
2024-04-14 23:05:59 +0500

    Полноценная сцена с поддержкой физики Bullet #107

--- media/vault_111/shaders/sources/lighting/Specular.fs
+++ media/vault_111/shaders/sources/lighting/Specular.fs
@@ -7,6 +7,6 @@ uniform float MetallicFactor;
 vec3 GetSpecular(vec2 iuv)
 {
     vec3 specular = texture(Specular, iuv).rgb;
-    specular = vec3(specular.r * SpecularFactor, 1.0 - specular.g, specular.r * MetallicFactor);
+    specular = vec3(specular.r * SpecularFactor, clamp(1.0 - specular.g, 0.05, 1.0), specular.r * MetallicFactor);
     return clamp(specular, 0.0, 1.0);
 }
