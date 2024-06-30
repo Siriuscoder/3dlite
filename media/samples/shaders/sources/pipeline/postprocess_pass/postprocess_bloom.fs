af54bcf5f5c232b71d503e9807684e516e79e955
Nikita Korolev <siriusnick@gmail.com>
2024-06-30 18:14:11 +0500

    Добавление дизеринга в поспроцессинг

--- media/samples/shaders/sources/pipeline/postprocess_pass/postprocess_bloom.fs
+++ media/samples/shaders/sources/pipeline/postprocess_pass/postprocess_bloom.fs
@@ -1,11 +1,8 @@
 #include "samples:shaders/sources/common/version.def"
+#include "samples:shaders/sources/common/utils_inc.glsl"
 
 uniform sampler2D Combined;
 uniform sampler2D Bloom;
-uniform float Gamma;
-uniform float Exposure;
-uniform float Contrast;
-uniform float Saturation;
 uniform vec3 ScreenResolution;
 
 out vec4 fragColor;
@@ -24,60 +21,20 @@ vec4 fxaa(sampler2D tex, vec2 fragCoord, vec2 resolution,
     vec2 v_rgbSW, vec2 v_rgbSE, 
     vec2 v_rgbM); 
 
-// Reinhard tone mapping
-vec3 ReinhardTonemapping(vec3 x)
-{
-    return x / (x + 1.0);
-}
-
-// Exposure tone mapping
-vec3 ExposureTonemapping(vec3 x, float e)
-{
-    return 1.0 - exp(-x * e);
-}
-
-mat4 contrastMatrix(float c)
-{
-	float t = (1.0 - c) / 2.0;
-    return mat4(c, 0, 0, 0,
-                0, c, 0, 0,
-                0, 0, c, 0,
-                t, t, t, 1);
-}
-
-mat4 saturationMatrix(float s)
-{
-    vec3 luminance = vec3(0.2126, 0.7152, 0.0722);
-    float oneMinusSat = 1.0 - s;
-
-    vec3 red = vec3(luminance.x * oneMinusSat);
-    red += vec3(s, 0, 0);
-    
-    vec3 green = vec3(luminance.y * oneMinusSat);
-    green += vec3(0, s, 0);
-    
-    vec3 blue = vec3(luminance.z * oneMinusSat);
-    blue += vec3(0, 0, s);
-    
-    return mat4(red,     0,
-                green,   0,
-                blue,    0,
-                0, 0, 0, 1);
-}
-
 void main()
 {
     // FSAA 
     vec3 hdr = fxaa(Combined, iuv * ScreenResolution.xy, ScreenResolution.xy, irgbNW, irgbNE, irgbSW, irgbSE, irgbM).rgb;
-    // Apply bloom
-    vec3 blm = texture(Bloom, iuv).rgb;
-    hdr = mix(hdr, blm, vec3(BloomStrength));
+    // BLOOM
+    hdr = mix(hdr, texture(Bloom, iuv).rgb, BloomStrength);
     // Exposure tone mapping
-    vec4 ldr = vec4(ExposureTonemapping(hdr, clamp(Exposure, 0.1, 3.5)), 1.0);
+    vec4 ldr = vec4(exposureTonemapping(hdr), 1.0);
     // Color correction
-    ldr = contrastMatrix(Contrast) * saturationMatrix(Saturation) * ldr;
+    ldr = contrastMatrix() * saturationMatrix() * ldr;
     // Gamma correction 
-    ldr.rgb = pow(ldr.rgb, vec3(1.0 / Gamma));
+    ldr.rgb = linearToSRGB(ldr.rgb);
+    // Dithering
+    ldr.rgb = ditherBayer(ldr.rgb);
     // Final Color
     fragColor = vec4(ldr.rgb, 1.0);
 }
